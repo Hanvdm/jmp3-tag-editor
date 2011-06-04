3848c4083091823e2872ce7bc3834077b43b8a42
giuseppemiscione@gmail.com <giuseppemiscione@gmail.com@2aba4466-dcc3-d6a6-0bb1-2b8f060e6cc1>
2011-06-04 10:50:29 +0000

    Moved to version 1.1.2. Added to possibility to specify an album cover to insert in the ID3v2 tag. Resolved some small bug in the UI management.

---
+++ tags/jMP3TagEditor/1.1.2/src/main/java/com/mscg/jmp3/ui/frame/MainWindow.java
@@ -0,0 +1,226 @@
+package com.mscg.jmp3.ui.frame;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
+import java.io.FileNotFoundException;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
+import javax.swing.border.EtchedBorder;
+
+import com.mscg.jmp3.i18n.Messages;
+import com.mscg.jmp3.main.AppLaunch;
+import com.mscg.jmp3.settings.Settings;
+import com.mscg.jmp3.theme.ThemeManager;
+import com.mscg.jmp3.theme.ThemeManager.IconType;
+import com.mscg.jmp3.ui.listener.CloseWindowClickListener;
+import com.mscg.jmp3.ui.panel.EncodeFileCard;
+import com.mscg.jmp3.ui.panel.FileChooseCard;
+import com.mscg.jmp3.ui.panel.FilenameOperationsCard;
+
+public class MainWindow extends JFrame implements ActionListener, ComponentListener, WindowStateListener {
+
+    private static final long serialVersionUID = -2803783276903439771L;
+
+    //private static Logger LOG = Logger.getLogger(MainWindow.class);
+
+    private JButton nextButton;
+    private JButton prevButton;
+    private JPanel mainPanel;
+
+    int cardIndex;
+    int cardsCount;
+    private boolean maximized;
+
+    private FileChooseCard fileChooseCard;
+    private FilenameOperationsCard filenameOperationsCard;
+    private EncodeFileCard encodeFileCard;
+
+    public MainWindow() throws FileNotFoundException {
+        this.maximized = Boolean.parseBoolean(Settings.getSetting("window.maximixed"));
+
+        //Use OS LookAndFeel
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+            initComponents();
+
+            SwingUtilities.updateComponentTreeUI(this);
+            pack();
+
+            // center on screen
+            setLocationRelativeTo(null);
+
+            if(maximized)
+                setExtendedState(getExtendedState() | MAXIMIZED_BOTH);
+        } catch(Exception e){
+            AppLaunch.manageError(e);
+        }
+    }
+
+    private void initComponents() throws FileNotFoundException {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle(Messages.getString("application.title"));
+        setIconImage(Toolkit.getDefaultToolkit().getImage(ThemeManager.getMainIcon(true)));
+
+        getContentPane().setLayout(new BorderLayout());
+
+        setMinimumSize(new Dimension(420, 280));
+
+        // panel for next/prev buttons
+        JPanel buttonBorderPanel = new JPanel(new BorderLayout());
+        buttonBorderPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+        buttonBorderPanel.add(buttonPanel);
+        nextButton = new JButton(Messages.getString("application.next"),
+                                 new ImageIcon(ThemeManager.getIcon(IconType.NEXT)));
+        nextButton.setPreferredSize(new Dimension(130, 36));
+        nextButton.setHorizontalTextPosition(SwingConstants.LEFT);
+        nextButton.addActionListener(this);
+        prevButton = new JButton(Messages.getString("application.prev"),
+                                 new ImageIcon(ThemeManager.getIcon(IconType.PREV)));
+        prevButton.addActionListener(this);
+        prevButton.setPreferredSize(new Dimension(130, 36));
+        prevButton.setVisible(false);
+        buttonPanel.add(Box.createHorizontalGlue());
+        buttonPanel.add(prevButton);
+        buttonPanel.add(nextButton);
+        getContentPane().add(buttonBorderPanel, BorderLayout.PAGE_END);
+
+        // menubar
+        JMenuBar menubar = new JMenuBar();
+        menubar.setPreferredSize(new Dimension(200, 20));
+        JMenu fileMenu = (JMenu)createMenu(Messages.getString("menu.file"), true);
+        JMenuItem exitMenu = createMenu(Messages.getString("menu.file.exit"), false);
+        exitMenu.setIcon(new ImageIcon(ThemeManager.getIcon(IconType.EXIT)));
+        exitMenu.addActionListener(new CloseWindowClickListener(MainWindow.this));
+        fileMenu.add(exitMenu);
+        menubar.add(fileMenu);
+        setJMenuBar(menubar);
+
+        int width = 620;
+        try {
+            width = Integer.parseInt(Settings.getSetting("mainarea.width"));
+        } catch(Exception e){}
+        int height = 462;
+        try {
+            height = Integer.parseInt(Settings.getSetting("mainarea.height"));
+        } catch(Exception e){}
+
+        mainPanel = new JPanel(new CardLayout());
+        mainPanel.setPreferredSize(new Dimension(width, height));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        getContentPane().add(mainPanel, BorderLayout.CENTER);
+
+        // add the cards to the main panel
+        cardIndex = 0;
+        fileChooseCard = new FileChooseCard(this);
+        mainPanel.add(fileChooseCard, "0");
+        filenameOperationsCard = new FilenameOperationsCard(this);
+        mainPanel.add(filenameOperationsCard, "1");
+        encodeFileCard = new EncodeFileCard(this);
+        mainPanel.add(encodeFileCard, "2");
+        cardsCount = mainPanel.getComponentCount();
+
+        addComponentListener(this);
+        addWindowStateListener(this);
+    }
+
+    private JMenuItem createMenu(String title, boolean isMenu) {
+        String realTitle = title;
+        Character mnemonic = null;
+        int index = title.indexOf('&');
+        if(index >= 0) {
+            mnemonic = title.charAt(index + 1);
+            realTitle = title.substring(0, index) + title.substring(index + 1);
+        }
+        JMenuItem menu = null;
+        if(isMenu)
+            menu = new JMenu(realTitle);
+        else
+            menu = new JMenuItem(realTitle);
+        if(mnemonic != null)
+            menu.setMnemonic(mnemonic);
+        return menu;
+    }
+
+    public JButton getNextButton() {
+        return nextButton;
+    }
+
+    public JButton getPrevButton() {
+        return prevButton;
+    }
+
+    public FileChooseCard getFileChooseCard() {
+        return fileChooseCard;
+    }
+
+    public FilenameOperationsCard getFilenameOperationsCard() {
+        return filenameOperationsCard;
+    }
+
+    //------------------------------------------------------------
+    // EVENTS
+    //------------------------------------------------------------
+    public void actionPerformed(ActionEvent e) {
+        CardLayout cardLayout = (CardLayout)mainPanel.getLayout();
+        if(nextButton == e.getSource()) {
+            cardIndex = (cardIndex + 1) % cardsCount;
+        } else {
+            cardIndex = (cardIndex + cardsCount - 1) % cardsCount;
+        }
+        cardLayout.show(mainPanel, Integer.toString(cardIndex));
+        prevButton.setVisible(cardIndex != 0);
+        nextButton.setVisible(cardIndex != (cardsCount - 1));
+    }
+
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    public void componentResized(ComponentEvent e) {
+        if(!maximized) {
+            Rectangle size = mainPanel.getBounds();
+            Settings.setSetting("mainarea.width", Integer.toString(size.width));
+            Settings.setSetting("mainarea.height", Integer.toString(size.height));
+        }
+    }
+
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    public void componentHidden(ComponentEvent e) {
+
+    }
+
+    public void windowStateChanged(WindowEvent e) {
+        maximized = (MAXIMIZED_BOTH & e.getNewState()) != 0;
+        Settings.setSetting("window.maximixed", Boolean.toString(maximized));
+    }
+
+}
