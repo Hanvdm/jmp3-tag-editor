69525ba97ec1b08d0d1a67f1e80433a59e40723d
giuseppemiscione <giuseppemiscione@gmail.com>
2011-06-05 10:51:14 +0000

    Added to function to load default settings from the classpath

--- jMP3TagEditor/src/main/java/com/mscg/jmp3/settings/Settings.java
+++ jMP3TagEditor/src/main/java/com/mscg/jmp3/settings/Settings.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,10 +25,23 @@ public class Settings {
         settings = new Properties();
         InputStream is = null;
         try {
+            Properties defaultProperites = new Properties();
+            LOG.debug("Loading default properties...");
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream("com/mscg/jmp3/conf/settings.properties");
+            if(is != null) {
+                defaultProperites.load(is);
+                IOUtils.closeQuietly(is);
+            }
+            LOG.info("Default properties loaded successfully");
             LOG.debug("Loading settings from file \"" + settingsFileName + "\"...");
             File settingsFile = new File(settingsFileName);
-            is = new FileInputStream(settingsFile);
-            settings.load(is);
+            Properties userProperties = new Properties();
+            try {
+                is = new FileInputStream(settingsFile);
+                userProperties.load(is);
+            } catch(Exception e){}
+            settings.putAll(defaultProperites);
+            settings.putAll(userProperties);
             LOG.info("Settings loaded successfully");
             Runtime.getRuntime().addShutdownHook(new ShutdownSaver());
         } finally {
@@ -54,6 +68,10 @@ public class Settings {
             try {
                 LOG.debug("Saving settings to file \"" + settingsFileName + "\"...");
                 File settingsFile = new File(settingsFileName);
+                if(!settingsFile.exists()) {
+                    FileUtils.forceMkdir(settingsFile.getParentFile());
+                    settingsFile.createNewFile();
+                }
                 os = new FileOutputStream(settingsFile);
                 settings.store(os, "jMP3TAG settings");
                 LOG.info("Settings saved successfully");
