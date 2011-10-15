2fafbcf4847e80d86f446b6854c2a81bf7ba79c9
giuseppemiscione@gmail.com <giuseppemiscione@gmail.com@2aba4466-dcc3-d6a6-0bb1-2b8f060e6cc1>
2011-10-15 15:19:19 +0000

    Localization resource bundle loaded from also from modules

--- jMP3i18n/src/main/java/com/mscg/jmp3/i18n/Messages.java
+++ jMP3i18n/src/main/java/com/mscg/jmp3/i18n/Messages.java
@@ -1,13 +1,17 @@
 package com.mscg.jmp3.i18n;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import java.util.Properties;
 
 public class Messages {
     private static final String BUNDLE_NAME = "com.mscg.jmp3.i18n.messages";
 
-    private static ResourceBundle RESOURCE_BUNDLE;
+    private static Properties RESOURCE_BUNDLE;
 
     public static void reloadBundle(String localeName) {
         Locale locale = null;
@@ -23,17 +27,67 @@ public class Messages {
             if(locale == null)
                 locale = Locale.getDefault();
         }
-        RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+
+        RESOURCE_BUNDLE = new Properties();
+
+        try {
+            for(String bundle : getBundleNames(locale)) {
+                Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources(bundle);
+                InputStream is = null;
+                while(resources.hasMoreElements()) {
+                    try {
+                        URL res = resources.nextElement();
+                        is = res.openStream();
+                        Properties tmp = new Properties();
+                        tmp.load(is);
+                        RESOURCE_BUNDLE.putAll(tmp);
+                    } finally {
+                        try {
+                            is.close();
+                        } catch(Exception e){}
+                    }
+                }
+            }
+
+        } catch(Exception e) {
+            throw new RuntimeException("Cannot initialize localization properties", e);
+        }
+
+    }
+
+    private static List<String> getBundleNames(Locale locale) {
+        List<String> bundles = new LinkedList<String>();
+
+        String bundleName = BUNDLE_NAME;
+        bundleName = bundleName.replace('.', '/') + ".properties";
+        bundles.add(bundleName);
+
+        bundleName = BUNDLE_NAME;
+        String localeSuffix = locale.getLanguage();
+        if (localeSuffix.length() > 0)
+            bundleName += "_" + localeSuffix;
+        bundleName = bundleName.replace('.', '/') + ".properties";
+        bundles.add(bundleName);
+
+        bundleName = BUNDLE_NAME;
+        localeSuffix = locale.toString();
+        if (localeSuffix.length() > 0)
+            bundleName += "_" + localeSuffix;
+        else if (locale.getVariant().length() > 0)
+            bundleName += "___" + locale.getVariant();
+        bundleName = bundleName.replace('.', '/') + ".properties";
+        bundles.add(bundleName);
+
+        return bundles;
     }
 
     private Messages() {
     }
 
     public static String getString(String key) {
-        try {
-            return RESOURCE_BUNDLE.getString(key);
-        } catch (MissingResourceException e) {
-            return '!' + key + '!';
-        }
+        String ret = RESOURCE_BUNDLE.getProperty(key);
+        if(ret == null)
+            ret = "!" + key + "!";
+        return ret;
     }
 }
