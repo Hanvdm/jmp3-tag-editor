5d495095c25a366ef722cf6c5dc74b82a7924ed0
giuseppemiscione@gmail.com <giuseppemiscione@gmail.com@2aba4466-dcc3-d6a6-0bb1-2b8f060e6cc1>
2011-10-15 10:56:42 +0000

    Classes moved to module projects

---
+++ jMP3i18n/src/main/java/com/mscg/jmp3/i18n/Messages.java
@@ -0,0 +1,39 @@
+package com.mscg.jmp3.i18n;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Messages {
+    private static final String BUNDLE_NAME = "com.mscg.jmp3.i18n.messages";
+
+    private static ResourceBundle RESOURCE_BUNDLE;
+
+    public static void reloadBundle(String localeName) {
+        Locale locale = null;
+        if("default".equalsIgnoreCase(localeName))
+            locale = Locale.getDefault();
+        else {
+            for(Locale availLocale : Locale.getAvailableLocales()) {
+                if(localeName.equalsIgnoreCase(availLocale.getLanguage())) {
+                    locale = availLocale;
+                    break;
+                }
+            }
+            if(locale == null)
+                locale = Locale.getDefault();
+        }
+        RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+    }
+
+    private Messages() {
+    }
+
+    public static String getString(String key) {
+        try {
+            return RESOURCE_BUNDLE.getString(key);
+        } catch (MissingResourceException e) {
+            return '!' + key + '!';
+        }
+    }
+}
