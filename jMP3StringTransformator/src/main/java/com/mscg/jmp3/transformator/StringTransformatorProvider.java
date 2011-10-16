9e54c8a7875b7d6341a9ec0186cd09fea5300b02
giuseppemiscione <giuseppemiscione@gmail.com>
2011-10-16 13:46:51 +0000

    Tag version 1.0.0

--- jMP3StringTransformator/src/main/java/com/mscg/jmp3/transformator/StringTransformatorProvider.java
+++
@@ -1,9 +0,0 @@
-package com.mscg.jmp3.transformator;
-
-import java.util.Map;
-
-public interface StringTransformatorProvider {
-
-    public Map<Integer, Class<? extends StringTransformator>> getStringTransformators();
-
-}
