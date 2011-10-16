15ce8f30ab4a306cc97d3462ba8c8b8ba95d51ef
giuseppemiscione@gmail.com <giuseppemiscione@gmail.com@2aba4466-dcc3-d6a6-0bb1-2b8f060e6cc1>
2011-10-16 10:04:17 +0000

    Tags will be updated

--- tags/jMP3StringTransformator/1.0.0/src/main/java/com/mscg/jmp3/transformator/StringTransformatorProvider.java
+++
@@ -1,9 +0,0 @@
-package com.mscg.jmp3.transformator;
-
-import java.util.List;
-
-public interface StringTransformatorProvider {
-
-    public List<Class<? extends StringTransformator>> getStringTransformators();
-
-}
