ffd7b6cd5e641dd4cb5234168a38a5b861152da4
giuseppemiscione <giuseppemiscione@gmail.com>
2011-10-16 10:05:30 +0000

    Tag version 1.0.0

--- jMP3TagEditor/src/main/java/com/mscg/jmp3/ui/listener/ListIndexAwareListener.java
+++
@@ -1,9 +0,0 @@
-package com.mscg.jmp3.ui.listener;
-
-import java.awt.event.ActionListener;
-
-public interface ListIndexAwareListener extends ActionListener {
-
-    public void setListIndex(Integer index);
-
-}
