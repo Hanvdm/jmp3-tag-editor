f90646257b3adaca26942accde47bdd4f89a5193
giuseppemiscione <giuseppemiscione@gmail.com>
2011-11-06 11:22:30 +0000

    Refactory of the main UI classes

--- radiodeejay.reloaded/radiodeejay.reloaded.main/src/main/java/com/mscg/main/ui/MainUIManager.java
+++ radiodeejay.reloaded/radiodeejay.reloaded.main/src/main/java/com/mscg/main/ui/MainUIManager.java
@@ -1,42 +1,11 @@
 package com.mscg.main.ui;
 
-import java.awt.EventQueue;
-import java.awt.Toolkit;
+public interface MainUIManager {
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+    public void showError(Exception e);
 
-import com.mscg.main.ui.impl.MainWindow;
-import com.mscg.main.util.ui.contextmenu.cutandpaste.CutAndPasteEventQueue;
+    public void showMessage(String title, String message);
 
-public class MainUIManager {
-
-    private static final Logger LOG = LoggerFactory.getLogger(MainUIManager.class);
-
-    public static void startInterface() {
-        try {
-            Toolkit.getDefaultToolkit().getSystemEventQueue().push(new CutAndPasteEventQueue());
-
-            EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                    try {
-                        MainWindowInterface.setInstance(new MainWindow());
-                        MainWindowInterface.getInstance().setVisible(true);
-                    } catch (Exception e) {
-                        manageError(e);
-                    }
-                 }
-             });
-
-        } catch(Exception e) {
-            manageError(e);
-        }
-    }
-
-    public static void manageError(Exception e) {
-        LOG.error("An error occurred and the application will close.", e);
-        MainWindowInterface.showError(e);
-        System.exit(1);
-    }
+    public void startInterface();
 
 }
