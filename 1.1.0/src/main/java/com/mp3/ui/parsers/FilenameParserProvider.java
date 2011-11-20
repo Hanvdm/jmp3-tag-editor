a24aba9048776fc8822a458fcbf20234913e06b9
giuseppemiscione <giuseppemiscione@gmail.com>
2011-11-20 12:58:48 +0000

    Tag version 1.1.0 renewed

---
+++ 1.1.0/src/main/java/com/mp3/ui/parsers/FilenameParserProvider.java
@@ -0,0 +1,30 @@
+package com.mp3.ui.parsers;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.mscg.jmp3.util.service.ServiceLoader;
+
+public class FilenameParserProvider {
+
+    private static Map<String, FilenamePatternParser> parsers;
+
+    public static synchronized Map<String, FilenamePatternParser> getParsers() throws ExistingParserIDException {
+        if(parsers == null) {
+            initParsers();
+        }
+        return parsers;
+    }
+
+    private static void initParsers() throws ExistingParserIDException {
+        ServiceLoader<FilenamePatternParser> serviceLoader = ServiceLoader.load(FilenamePatternParser.class);
+        parsers = new TreeMap<String, FilenamePatternParser>();
+        for(FilenamePatternParser parser : serviceLoader) {
+            String parserID = parser.getParserID();
+            if(parsers.containsKey(parserID))
+                throw new ExistingParserIDException("A parser with ID \"" + parserID + "\" has been " +
+                		                            "already registered");
+            parsers.put(parserID, parser);
+        }
+    }
+}
