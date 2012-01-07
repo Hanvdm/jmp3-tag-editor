813379ec065e3b66162992419d4ff3999e0ec3b6
giuseppemiscione@gmail.com <giuseppemiscione@gmail.com@2aba4466-dcc3-d6a6-0bb1-2b8f060e6cc1>
2012-01-07 11:56:29 +0000

    ASF object GUID wrapper

---
+++ trunk/jWMATag/src/main/java/com/mscg/asf/exception/GUIDSizeException.java
@@ -0,0 +1,23 @@
+package com.mscg.asf.exception;
+
+public class GUIDSizeException extends GUIDException {
+
+    private static final long serialVersionUID = 5840029502965345626L;
+
+    public GUIDSizeException() {
+        super();
+    }
+
+    public GUIDSizeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GUIDSizeException(String message) {
+        super(message);
+    }
+
+    public GUIDSizeException(Throwable cause) {
+        super(cause);
+    }
+
+}
