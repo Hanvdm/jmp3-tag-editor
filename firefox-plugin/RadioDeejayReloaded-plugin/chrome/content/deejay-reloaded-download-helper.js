c562cafc0ef0cd72a35a2a487abed30b3ea01a09
giuseppemiscione <giuseppemiscione@gmail.com>
2012-02-22 21:52:30 +0000

    Implemented save action

---
+++ firefox-plugin/RadioDeejayReloaded-plugin/chrome/content/deejay-reloaded-download-helper.js
@@ -0,0 +1,156 @@
+function RadioDeejayReloadedDownloadHelper() {
+
+}
+
+RadioDeejayReloadedDownloadHelper.prototype = {
+    // Helper function to wait for appropriate MIME-type headers and
+    // then prompt the user with a file picker
+    saveHelper : function(linkURL, linkText, dialogTitle, bypassCache, doc) {
+        // canonical def in nsURILoader.h
+        const
+        NS_ERROR_SAVE_LINK_AS_TIMEOUT = 0x805d0020;
+
+        // an object to proxy the data through to
+        // nsIExternalHelperAppService.doContent, which will wait for the
+        // appropriate MIME-type headers and then prompt the user with a
+        // file picker
+        function saveAsListener() {
+        }
+        ;
+        saveAsListener.prototype = {
+            extListener : null,
+
+            onStartRequest : function saveLinkAs_onStartRequest(aRequest,
+                    aContext) {
+
+                // if the timer fired, the error status will have been caused by
+                // that,
+                // and we'll be restarting in onStopRequest, so no reason to
+                // notify
+                // the user
+                if (aRequest.status == NS_ERROR_SAVE_LINK_AS_TIMEOUT)
+                    return;
+
+                timer.cancel();
+
+                // some other error occured; notify the user...
+                if (!Components.isSuccessCode(aRequest.status)) {
+                    try {
+                        const
+                        sbs = Cc["@mozilla.org/intl/stringbundle;1"]
+                                .getService(Ci.nsIStringBundleService);
+                        const
+                        bundle = sbs
+                                .createBundle("chrome://mozapps/locale/downloads/downloads.properties");
+
+                        const
+                        title = bundle
+                                .GetStringFromName("downloadErrorAlertTitle");
+                        const
+                        msg = bundle.GetStringFromName("downloadErrorGeneric");
+
+                        const
+                        promptSvc = Cc["@mozilla.org/embedcomp/prompt-service;1"]
+                                .getService(Ci.nsIPromptService);
+                        promptSvc.alert(doc.defaultView, title, msg);
+                    } catch (ex) {
+                    }
+                    return;
+                }
+
+                var extHelperAppSvc = Cc["@mozilla.org/uriloader/external-helper-app-service;1"]
+                        .getService(Ci.nsIExternalHelperAppService);
+                var channel = aRequest.QueryInterface(Ci.nsIChannel);
+                this.extListener = extHelperAppSvc.doContent(
+                        channel.contentType, aRequest, doc.defaultView, true);
+                this.extListener.onStartRequest(aRequest, aContext);
+            },
+
+            onStopRequest : function saveLinkAs_onStopRequest(aRequest,
+                    aContext, aStatusCode) {
+                if (aStatusCode == NS_ERROR_SAVE_LINK_AS_TIMEOUT) {
+                    // do it the old fashioned way, which will pick the best
+                    // filename
+                    // it can without waiting.
+                    saveURL(linkURL, linkText, dialogTitle, bypassCache, false,
+                            doc.documentURIObject);
+                }
+                if (this.extListener)
+                    this.extListener.onStopRequest(aRequest, aContext,
+                            aStatusCode);
+            },
+
+            onDataAvailable : function saveLinkAs_onDataAvailable(aRequest,
+                    aContext, aInputStream, aOffset, aCount) {
+                this.extListener.onDataAvailable(aRequest, aContext,
+                        aInputStream, aOffset, aCount);
+            }
+        }
+
+        function callbacks() {
+        }
+        callbacks.prototype = {
+            getInterface : function sLA_callbacks_getInterface(aIID) {
+                if (aIID.equals(Ci.nsIAuthPrompt)
+                        || aIID.equals(Ci.nsIAuthPrompt2)) {
+                    // If the channel demands authentication prompt, we must
+                    // cancel it
+                    // because the save-as-timer would expire and cancel the
+                    // channel
+                    // before we get credentials from user. Both authentication
+                    // dialog
+                    // and save as dialog would appear on the screen as we fall
+                    // back to
+                    // the old fashioned way after the timeout.
+                    timer.cancel();
+                    channel.cancel(NS_ERROR_SAVE_LINK_AS_TIMEOUT);
+                }
+                throw Cr.NS_ERROR_NO_INTERFACE;
+            }
+        }
+
+        // if it we don't have the headers after a short time, the user
+        // won't have received any feedback from their click. that's bad. so
+        // we give up waiting for the filename.
+        function timerCallback() {
+        }
+        timerCallback.prototype = {
+            notify : function sLA_timer_notify(aTimer) {
+                channel.cancel(NS_ERROR_SAVE_LINK_AS_TIMEOUT);
+                return;
+            }
+        }
+
+        // set up a channel to do the saving
+        var ioService = Cc["@mozilla.org/network/io-service;1"]
+                .getService(Ci.nsIIOService);
+        var channel = ioService.newChannelFromURI(makeURI(linkURL));
+        channel.notificationCallbacks = new callbacks();
+
+        var flags = Ci.nsIChannel.LOAD_CALL_CONTENT_SNIFFERS;
+
+        if (bypassCache)
+            flags |= Ci.nsIRequest.LOAD_BYPASS_CACHE;
+
+        if (channel instanceof Ci.nsICachingChannel)
+            flags |= Ci.nsICachingChannel.LOAD_BYPASS_LOCAL_CACHE_IF_BUSY;
+
+        channel.loadFlags |= flags;
+
+        if (channel instanceof Ci.nsIHttpChannel) {
+            channel.referrer = doc.documentURIObject;
+            if (channel instanceof Ci.nsIHttpChannelInternal)
+                channel.forceAllowThirdPartyCookie = true;
+        }
+
+        // fallback to the old way if we don't see the headers quickly
+        var timeToWait = gPrefService
+                .getIntPref("browser.download.saveLinkAsFilenameTimeout");
+        var timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+        timer.initWithCallback(new timerCallback(), timeToWait,
+                timer.TYPE_ONE_SHOT);
+
+        // kick off the channel with our proxy object as the listener
+        channel.asyncOpen(new saveAsListener(), null);
+    }
+}
--- firefox-plugin/RadioDeejayReloaded-plugin/chrome/content/deejay-reloaded.js
+++ firefox-plugin/RadioDeejayReloaded-plugin/chrome/content/deejay-reloaded.js
@@ -3,10 +3,14 @@ function RadioDeejayReloadedDownload() {
     this.popupMenuItem = null;
 }
 
+RadioDeejayReloadedDownload.urlPatter = new RegExp("http://.*\\.?deejay\.it/dj/radio/programma/reloaded/\\d+/\\d{4}-\\d{2}-\\d{2}/.+");
+RadioDeejayReloadedDownload.downloadPatter = new RegExp("createPlayerFLW\\(.+'(http://.*)',.+\\)");
+RadioDeejayReloadedDownload.downloadHelper = new RadioDeejayReloadedDownloadHelper();
+
 RadioDeejayReloadedDownload.prototype = {
     menuItem: null,
     popupMenuItem: null,
-    
+
     init: function() {
         var me = this;
 
@@ -18,6 +22,7 @@ RadioDeejayReloadedDownload.prototype = {
         gBrowser.tabContainer.addEventListener("TabSelect", function(event) {
             me.onTabSelection(event);
         }, false);
+        this.checkTabLocation(gBrowser.getBrowserForTab(gBrowser.selectedTab).currentURI.spec);
     },
 
     onPageLoad: function(event) {
@@ -25,44 +30,45 @@ RadioDeejayReloadedDownload.prototype = {
         if (!win || win.frameElement) {  
             return;
         }
-        this.checkTabLocation(win.location.host, win.location.pathname);
+        this.checkTabLocation(win.location.toString());
     },
 
     onTabSelection: function(event) {
         if(event && event.originalTarget) {
             var browser = gBrowser.getBrowserForTab(event.originalTarget);
-            var host = null;
-            try {
-                host = browser.currentURI.host;
-            } catch(e){}
-            var path = null;
-            try {
-                path = browser.currentURI.path;
-            } catch(e){}
-            this.checkTabLocation(host, path);
+            this.checkTabLocation(browser.currentURI.spec);
         }
     },
 
-    checkTabLocation: function(host, path) {
-        if(!host || !path) {
-            this.menuItem.disabled = true;
-            this.popupMenuItem.disabled = true;
-        }
+    checkTabLocation: function(pageUrl) {
+        if(!pageUrl)
+            this.setMenuEnabledStatus(false)
         else {
-            if(host.lastIndexOf("deejay.it") >= 0 && path.indexOf("/dj/radio/reloaded") >= 0) {
-                this.menuItem.disabled = false;
-                this.popupMenuItem.disabled = false;
-            }
-            else {
-                this.menuItem.disabled = true;
-                this.popupMenuItem.disabled = true;
-            }
+            var match = RadioDeejayReloadedDownload.urlPatter.exec(pageUrl);
+            if(match != null)
+                this.setMenuEnabledStatus(true);
+            else
+                this.setMenuEnabledStatus(false);
         }
+    },
+    
+    setMenuEnabledStatus: function(enabled) {
+        this.menuItem.disabled = !enabled;
+        this.popupMenuItem.disabled = !enabled;
+        this.popupMenuItem.hidden = !enabled;
     },
 
     startDownload: function() {
         if(!this.menuItem.disabled) {
-            alert("Starting download");
+            var xPathScript = content.document.evaluate("//div[@id='mediaspace']/parent::dt/script/text()",
+            		                                    content.document, null, XPathResult.ANY_TYPE, null) ;
+            var script = xPathScript.iterateNext().textContent;
+            var match = RadioDeejayReloadedDownload.downloadPatter.exec(script);
+            if(match != null && match.length >= 2) {
+            	var url = match[1];
+            	RadioDeejayReloadedDownload.downloadHelper.saveHelper(url, "testo", null,
+            			                                              true, content.document);
+            }
         }
     }
 }
