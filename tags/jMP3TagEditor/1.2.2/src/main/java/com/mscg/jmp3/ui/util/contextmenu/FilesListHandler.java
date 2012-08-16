917d72f3fa9391e69879d4e7f08c0b49dab8232c
giuseppemiscione@gmail.com <giuseppemiscione@gmail.com@2aba4466-dcc3-d6a6-0bb1-2b8f060e6cc1>
2012-08-16 14:26:07 +0000

    Version 1.2.2

---
+++ tags/jMP3TagEditor/1.2.2/src/main/java/com/mscg/jmp3/ui/util/contextmenu/FilesListHandler.java
@@ -0,0 +1,35 @@
+package com.mscg.jmp3.ui.util.contextmenu;
+
+import java.awt.Desktop;
+import java.awt.event.MouseEvent;
+import java.io.File;
+
+import javax.swing.JList;
+
+import com.mp3.ui.MainWindowInterface;
+import com.mscg.jmp3.ui.renderer.elements.IconAndFileListElement;
+
+public class FilesListHandler extends ContextMenuListHandler {
+
+    public FilesListHandler(JList list, JPopupMenuFactory popupMenuFactory) {
+        super(list, popupMenuFactory);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent ev) {
+        if(ev.getClickCount() == 2 && list.getSelectedIndices().length == 1) {
+            int idx = list.getSelectedIndex();
+            if(idx < 0)
+                return;
+            IconAndFileListElement fileElem = (IconAndFileListElement)list.getSelectedValue();
+            File file = fileElem.getFile();
+            try {
+                Desktop.getDesktop().open(fileElem.getFile().getCanonicalFile());
+            } catch (Exception e) {
+                LOG.warn("Cannot play file \"" + file.getAbsolutePath() + "\"", e);
+                MainWindowInterface.showError(e);
+            }
+        }
+    }
+
+}
