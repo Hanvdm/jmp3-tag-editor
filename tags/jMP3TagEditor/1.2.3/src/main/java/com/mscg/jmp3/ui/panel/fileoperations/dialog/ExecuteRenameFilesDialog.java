174f91a895f8d012a9e86c3db981c5820b9c2ea7
giuseppemiscione@gmail.com <giuseppemiscione@gmail.com@2aba4466-dcc3-d6a6-0bb1-2b8f060e6cc1>
2013-02-18 20:55:20 +0000

    Tag 1.2.3

---
+++ tags/jMP3TagEditor/1.2.3/src/main/java/com/mscg/jmp3/ui/panel/fileoperations/dialog/ExecuteRenameFilesDialog.java
@@ -0,0 +1,65 @@
+package com.mscg.jmp3.ui.panel.fileoperations.dialog;
+
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.io.FileNotFoundException;
+
+import com.mscg.jmp3.i18n.Messages;
+import com.mscg.jmp3.ui.panel.fileoperations.RenameFileTab;
+import com.mscg.jmp3.util.pool.InterruptibleRunnable;
+import com.mscg.jmp3.util.pool.runnable.RenameFilesRunnable;
+
+public class ExecuteRenameFilesDialog extends GenericFilesOperationDialog {
+
+    private static final long serialVersionUID = -3630221636283924977L;
+
+    private RenameFileTab tab;
+
+    public ExecuteRenameFilesDialog(RenameFileTab tab) throws FileNotFoundException {
+        super();
+        initComponents(tab);
+    }
+
+    public ExecuteRenameFilesDialog(RenameFileTab tab, Dialog owner, boolean modal) throws FileNotFoundException {
+        super(owner, modal);
+        initComponents(tab);
+    }
+
+    public ExecuteRenameFilesDialog(RenameFileTab tab, Dialog owner) throws FileNotFoundException {
+        super(owner);
+        initComponents(tab);
+    }
+
+    public ExecuteRenameFilesDialog(RenameFileTab tab, Frame owner, boolean modal) throws FileNotFoundException {
+        super(owner, modal);
+        initComponents(tab);
+    }
+
+    public ExecuteRenameFilesDialog(RenameFileTab tab, Frame owner) throws FileNotFoundException {
+        super(owner);
+        initComponents(tab);
+    }
+
+    protected void initComponents(RenameFileTab tab) throws FileNotFoundException {
+        this.tab = tab;
+        initComponents();
+    }
+
+    /**
+     * @return the tab
+     */
+    public RenameFileTab getTab() {
+        return tab;
+    }
+
+    @Override
+    protected String getDialogTitle() {
+        return Messages.getString("operations.file.rename.execute.title");
+    }
+
+    @Override
+    protected InterruptibleRunnable getRunnable() {
+        return new RenameFilesRunnable(this);
+    }
+
+}
